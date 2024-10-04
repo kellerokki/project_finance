616b16ac3619de518814ba0eb62ab937de694033
Rolle <rolle.kekki@protonmail.com>
2024-10-04 09:42:45 +0300

    Imported pandas, csv and datetime modules in main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+import pandas as pd #Allows to load in the csv file and work with it faster
+import csv #Imports csv file extension
+from datetime import datetime #Module built in Python, that allows to work with dates and times.
+
